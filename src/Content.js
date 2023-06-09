f750a12884b0aeafa7ea77335f9b96fa0c4d1f6c
Armstrong <Mario.Armstrong@10news.com>
2023-06-09 00:46:56 -0700

    worked on react and click events/buttons

--- src/Content.js
+++ src/Content.js
@@ -6,12 +6,31 @@ const Content = () => {
         const int = Math.floor(Math.random() *3);
         return names[int];
       }
-    
+
+      const handleClick = () => {
+        console.log('you clicked it!');
+      }
+      const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+      }
+      const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+      }
+    // to create a duplicate use SHIFT + ALT + downArrow
   return (
     <main>
-        <p>
+        <p
+        onDoubleClick={handleClick}
+        >
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click It</button>
+        
+        <button onClick={() => {handleClick2('Dave')}}>Click It</button>
+        {/* handleClick2 has the operators () but isn't called immediately because it is inside an anonymous function */}
+
+        <button onClick={(e) => {handleClick3(e)}}>Click It</button>
+
     </main>
   )
 }
